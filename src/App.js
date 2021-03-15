20db4fc4956067062e255eef9f1a897ef0cdee97
Rob Thijssen <rthijssen@gmail.com>
2021-03-15 15:53:40 +0200

    working draft

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import Cookies from 'universal-cookie';
+import moment from 'moment';
 
 // bootstrap
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
@@ -13,15 +16,37 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 
 import AssetTracker from './AssetTracker';
 
+const cookies = new Cookies();
+const colors = {
+  bch: '#8dc351',
+  btc: '#f7931a',
+  dash: '#008ce7',
+  dot: '#e71081',
+  eth: '#828384',
+  ltc: '#345d9d',
+  xlm: '#000000',
+  xrp: '#23292f',
+  zec: '#ecb244',
+};
+
 
 function App() {
-  const [gistId, setGistId] = useState(undefined);
+  const [dateRange, setDateRange] = useState({ from: moment('2020-11-11').toISOString().slice(0, 10), to: moment().toISOString().slice(0, 10) });
+  const [currency, setCurrency] = useState(cookies.get('currency') || 'eur');
+  const [gistId, setGistId] = useState(window.location.href.match(/#[a-f0-9]{32}$/) ? window.location.href.split('#').pop() : cookies.get('gist'));
   const [assetTracker, setAssetTracker] = useState(undefined);
   useEffect(() => {
-    setGistId('asdf');
-  }, []);
+    if(!!currency && currency.match(/^[a-z]{3}$/) && cookies.get('currency') !== currency) {
+      cookies.set('currency', currency, { path: '/' });
+    }
+  }, [currency]);
   useEffect(() => {
-    if (!!gistId) {
+    if(!!gistId && gistId.match(/^[a-f0-9]{32}$/) && cookies.get('gist') !== gistId) {
+      cookies.set('gist', gistId, { path: '/' });
+    }
+  }, [gistId]);
+  useEffect(() => {
+    if (!!gistId && gistId.match(/^[a-f0-9]{32}$/)) {
       AssetTracker
         .fromGist(gistId)
         .then(setAssetTracker)
@@ -31,74 +56,201 @@ function App() {
   return (
     <Container fluid>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">thgttg</Navbar.Brand>
+        <Navbar.Brand href="#">don't panic</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#about">about</Nav.Link>
         </Nav>
+        <Form inline>
+          <Form.Control
+            type="text"
+            style={{width: '7em'}}
+            className="mr-sm-2"
+            size="sm"
+            disabled={true}
+            value={dateRange.from}
+            onChange={
+              (event) => {
+                const from = event.target.value;
+                if (from.match(/^20[12][0-9]-[01][0-9]-[0123][0-9]$/)) {
+                  setDateRange(_dateRange => ({
+                    ..._dateRange,
+                    from
+                  }));
+                }
+              }
+            } />
+          <Form.Control
+            type="text"
+            style={{width: '7em'}}
+            className="mr-sm-2"
+            size="sm"
+            disabled={true}
+            value={dateRange.to}
+            onChange={
+              (event) => {
+                const to = event.target.value;
+                if (to.match(/^20[12][0-9]-[01][0-9]-[0123][0-9]$/)) {
+                  setDateRange(_dateRange => ({
+                    ..._dateRange,
+                    to
+                  }));
+                }
+              }
+            } />
+          <Form.Control
+            type="text"
+            placeholder="gist id"
+            style={{width: '19em'}}
+            className="mr-sm-2"
+            size="sm"
+            disabled={true}
+            defaultValue={gistId}
+            onChange={
+              (event) => {
+                const gistId = event.target.value;
+                if (gistId.match(/^[a-f0-9]{32}$/)) {
+                  setGistId(gistId);
+                }
+              }
+            } />
+          <Form.Control
+            as="select"
+            className="mr-sm-2"
+            size="sm"
+            disabled={true}
+            defaultValue={currency}
+            onChange={
+              (event) => {
+                const currency = event.target.value;
+                if (currency.match(/^[a-f]{3}$/)) {
+                  setCurrency(currency);
+                }
+              }
+            }>
+            <option>eur</option>
+            <option>usd</option>
+          </Form.Control>
+        </Form>
       </Navbar>
       <Row>
-        <Col>
-          {
-            (!!assetTracker && !!assetTracker.balances.length)
-              ? (
-                  <>
-                    <ResponsiveContainer width="100%" minHeight="400px">
-                      <LineChart data={assetTracker.balances.filter(b => b.btc > 0)} margin={{ top: 50, right: 10, left: 10, bottom: 10 }} >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="date" />
-                        <YAxis yAxisId="left" />
-                        <YAxis yAxisId="right" orientation="right" />
-                        <Tooltip />
-                        <Legend />
-                        <Line yAxisId="left" type="monotone" dataKey="dot" stroke="#82ca9d" />
-                        <Line yAxisId="left" type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                        <Line yAxisId="right" type="monotone" dataKey="uv" stroke="#82ca9d" />
-                      </LineChart>
-                    </ResponsiveContainer>
-                    <pre>
-                      {JSON.stringify(assetTracker.balances, null, 2)}
-                    </pre>
-                  </>
-                )
-              : null
-          }
-        </Col>
+        {
+          (!!assetTracker && !!assetTracker.balances.length)
+            ? (
+                <ResponsiveContainer width="100%" minHeight="400px">
+                  <LineChart
+                    data={assetTracker.balances.filter(balance => moment(balance.date).isBetween(moment(dateRange.from), moment(dateRange.to)))}
+                    margin={{ top: 50, right: 10, left: 10, bottom: 10 }} >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="date" />
+                    <YAxis yAxisId="left" />
+                    <Tooltip />
+                    <Legend />
+                    {
+                      assetTracker.assets.map(asset => (
+                        <Line yAxisId="left" type="monotone" dataKey={`${asset}.${currency}`} stroke={colors[asset] || '#82ca9d'} key={asset} />
+                      ))
+                    }
+                  </LineChart>
+                </ResponsiveContainer>
+              )
+            : null
+        }
       </Row>
       <Row>
-        <Col>
-          <Table striped bordered hover size="sm" style={{margin: '50px 10px 10px 10px'}}>
-            <thead>
-              <tr>
-                <th>date</th>
-                <th>asset</th>
-                <th>amount</th>
-                <th>note</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                (!!assetTracker && !!assetTracker.transactions.length)
-                  ? assetTracker.transactions.reverse().map((tx, key) => (
-                      <tr key={key}>
-                        <td>
-                          {new Intl.DateTimeFormat('en-GB').format(Date.parse(tx.date))}
-                        </td>
-                        <td>
-                          {tx.asset}
-                        </td>
-                        <td>
-                          {tx.amount}
-                        </td>
-                        <td>
-                          {tx.note}
-                        </td>
-                      </tr>
-                    ))
-                  : null
-              }
-            </tbody>
-          </Table>
-        </Col>
+        <Table striped bordered hover size="sm" variant="dark" style={{margin: '50px 10px 10px 10px'}}>
+          <thead>
+            <tr>
+              <th colspan="4" className="text-right">
+                transaction history
+              </th>
+            </tr>
+            <tr>
+              <th>
+                date
+              </th>
+              <th>
+                note
+              </th>
+              <th>
+                asset
+              </th>
+              <th className="text-right">
+                amount
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              (!!assetTracker && !!assetTracker.transactions.length)
+                ? assetTracker.transactions.filter(transaction => moment(transaction.date).isBetween(moment(dateRange.from), moment(dateRange.to))).map((tx, key) => (
+                    <tr key={key}>
+                      <td>
+                        {tx.date}
+                      </td>
+                      <td>
+                        {tx.note}
+                      </td>
+                      <td>
+                        {tx.asset}
+                      </td>
+                      <td className="text-right">
+                        {tx.amount}
+                      </td>
+                    </tr>
+                  ))
+                : null
+            }
+          </tbody>
+        </Table>
+      </Row>
+      <Row>
+        {
+          (!!assetTracker && !!assetTracker.balances.length)
+            ? (
+                <Table striped bordered hover size="sm" variant="dark" style={{margin: '50px 10px 10px 10px'}}>
+                  <thead>
+                    <tr>
+                      <th colspan={(assetTracker.assets.length + 2)} className="text-right">
+                        balance history
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>date</th>
+                      {
+                        assetTracker.assets.map(asset => (
+                          <th key={asset} className="text-right">
+                            {asset}
+                          </th>
+                        ))
+                      }
+                      <th className="text-right">total</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      assetTracker.balances.filter(balance => moment(balance.date).isBetween(moment(dateRange.from), moment(dateRange.to))).map((balance) => (
+                        <tr key={balance.date}>
+                          <th>{balance.date}</th>
+                          {
+                            assetTracker.assets.map(asset => (
+                              <td key={`${balance.date}-${asset}`} className="text-right">
+                                <span className="text-muted" style={{marginRight: '1em'}}>
+                                  {balance[asset][asset]}
+                                </span>
+                                {new Intl.NumberFormat('en-GB', { style: 'currency', currency: currency.toUpperCase() }).format(balance[asset][currency])}
+                              </td>
+                            ))
+                          }
+                          <td className="text-right">
+                            {new Intl.NumberFormat('en-GB', { style: 'currency', currency: currency.toUpperCase() }).format(assetTracker.assets.reduce((acc, asset) => acc + balance[asset][currency], 0))}
+                          </td>
+                        </tr>
+                      ))
+                    }
+                  </tbody>
+                </Table>
+              )
+            : null
+        }
       </Row>
     </Container>
   );
--- src/AssetTracker.js
+++ src/AssetTracker.js
@@ -1,27 +1,119 @@
+import BigNumber from "bignumber.js";
+import moment from 'moment';
+import {
+  Stitch,
+  AnonymousCredential,
+  RemoteMongoClient
+} from 'mongodb-stitch-browser-sdk';
+
+const appClient = Stitch.initializeDefaultAppClient('grenade-tnats');
+const svcClient = appClient.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas-grenade-stitch');
+
+const dateSorter = (a, b) => (
+    (moment(a.date) < moment(b.date))
+        ? -1
+        : (moment(a.date) > moment(b.date))
+            ? 1
+            : 0
+);
+
+const coinUnitMap = {
+    bch: {
+        monetary: {
+            unit: 'bch',
+        },
+        fractional: {
+            unit: 'satoshi',
+            decimalPlaces: 8,
+        },
+    },
+    btc: {
+        monetary: {
+            unit: 'btc',
+        },
+        fractional: {
+            unit: 'satoshi',
+            decimalPlaces: 8,
+        },
+    },
+    dash: {
+        monetary: {
+            unit: 'dash',
+        },
+        fractional: {
+            unit: 'duff',
+            decimalPlaces: 8,
+        },
+    },
+    eth: {
+        monetary: {
+            unit: 'eth',
+        },
+        fractional: {
+            unit: 'wei',
+            decimalPlaces: 18,
+        },
+    },
+    ltc: {
+        monetary: {
+            unit: 'ltc',
+        },
+        fractional: {
+            unit: 'litoshi',
+            decimalPlaces: 8,
+        },
+    },
+    xrp: {
+        monetary: {
+            unit: 'xrp',
+        },
+        fractional: {
+            unit: 'drop',
+            decimalPlaces: 8,
+        },
+    },
+    zec: {
+        monetary: {
+            unit: 'zec',
+        },
+        fractional: {
+            unit: 'zatoshi',
+            decimalPlaces: 8,
+        },
+    },
+};
+
 export default class AssetTracker {
-    constructor(transactionsCollections) {
-        this.transactions = transactionsCollections
-            .reduce((accumulator, transactions) => [...accumulator, ...transactions])
-            .sort((a, b) => (
-                (Date.parse(a.date) < Date.parse(b.date))
-                    ? -1
-                    : (Date.parse(a.date) > Date.parse(b.date))
-                        ? 1
-                        : 0
-            ));
-        this.earliestDate = Date.parse(this.transactions[0].date);
-        this.latestDate = Date.parse([...this.transactions].pop().date);
+    constructor(transactions, quotes) {
+        this.quotes = quotes;
+        this.transactions = transactions.sort(dateSorter);
+        this.earliestDate = moment(this.transactions[0].date);
+        this.latestDate = moment();
         this.assets = [...new Set(this.transactions.map(tx => tx.asset))].sort();
         this.balanceAtDate = (asset, date) => this.transactions
-            .filter(tx => ((tx.asset === asset) && (Date.parse(tx.date) <= date)))
-            .reduce((a, b) => a.amount + b.amount, 0);
+            .filter(tx => ((tx.asset === asset) && (moment(tx.date) <= date)))
+            .reduce((accumulator, tx) => accumulator.plus(new BigNumber(tx.amount)), new BigNumber(0));
         const interval = 1000 * 60 * 60 * 24; // one day
         this.balances = Array.from(
             { length: (((this.latestDate - this.earliestDate) / interval) + 1) },
-            (v, i) => new Date(this.earliestDate.valueOf() + (interval * i))
+            (v, i) => new Date(this.earliestDate.valueOf() + (interval * i)).toISOString().slice(0, 10)
         ).map(date => ({
-            ...Object.fromEntries(this.assets.map(asset => [asset, this.balanceAtDate(asset, date)])),
-            date: new Intl.DateTimeFormat('en-GB').format(date),
+            ...Object.fromEntries(this.assets.map(asset => {
+                const close = new Date(date);
+                close.setHours(23, 59, 59);
+                const balance = this.balanceAtDate(asset, close);
+                return [
+                    asset,
+                    {
+                        ...Object.fromEntries(quotes.filter(quote => ((quote.coin === asset) && (quote.date === date))).map(quote => [
+                            quote.fiat,
+                            balance.times(BigNumber(quote.open.amount).plus(BigNumber(quote.close.amount)).dividedBy(BigNumber(2))).toNumber()
+                        ])),
+                        [asset]: balance.toNumber(),
+                    }
+                ];
+            })),
+            date,
         }));
     }
 
@@ -30,10 +122,45 @@ export default class AssetTracker {
             fetch(`https://api.github.com/gists/${gistId}`)
                 .then((response) => response.json())
                 .then((gist) => {
-                    const transactionsCollections = Object.keys(gist.files)
+                    const transactions = Object.keys(gist.files)
                         .filter(_filename => _filename.endsWith('.json'))
-                        .map(_filename => JSON.parse(gist.files[_filename].content));
-                    resolve(new AssetTracker(transactionsCollections));
+                        .map(_filename => JSON.parse(gist.files[_filename].content))
+                        .reduce((accumulator, _transactions) => [...accumulator, ..._transactions])
+                        .filter(_transaction => Object.keys(coinUnitMap).includes(_transaction.asset))
+                        .sort(dateSorter);
+                    const earliestDate = transactions[0].date.slice(0, 10);
+                    const latestDate = moment().toISOString().slice(0, 10);
+                    appClient.auth
+                        .loginWithCredential(new AnonymousCredential())
+                        .then(() => {
+                            svcClient.db('grenade')
+                                .collection('quote')
+                                .find(
+                                    {
+                                        $and: [
+                                            {
+                                                coin: {
+                                                    $in: [...new Set(transactions.map(tx => tx.asset))]
+                                                }
+                                            },
+                                            {
+                                                date: {
+                                                    $gte: earliestDate
+                                                }
+                                            },
+                                            {
+                                                date: {
+                                                    $lte: latestDate
+                                                }
+                                            }
+                                        ]
+                                    }
+                                )
+                                .asArray()
+                                .then(quotes => {
+                                    resolve(new AssetTracker(transactions, quotes));
+                                });
+                        });
                 })
                 .catch((error) => {
                     reject(error);
